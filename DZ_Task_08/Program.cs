3135a8dc6cc1944aa3d88f275d5d5b76bb7a2cee
Rasim Alimov <rasim.alimov.88@mail.ru>
2022-12-05 10:15:11 +0700

    Решение задания 8

---
+++ DZ_Task_08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//4 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+string inputfirst = Console.ReadLine();
+int number = Convert.ToInt32(inputfirst);
+
+number = Math.Abs(number); //преобразователь введенного отрицательного числа в положительное
+
+for (int i = 2; i<=number; i=i+2)
+{
+    Console.Write(i+ " ");
+}
+

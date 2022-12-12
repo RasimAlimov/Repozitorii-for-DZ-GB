eb2f6577913796ecd59a1cfa3e827f739a3dafc5
RasimAlimov <119108570+RasimAlimov@users.noreply.github.com>
2022-12-12 09:47:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// // Программа принимает число N и выдает таблицу кубов от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<= number; i++)
+{
+    Console.WriteLine ($"{i*i*i}");
+}
